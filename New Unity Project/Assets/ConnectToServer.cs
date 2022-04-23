0ca7664dc0cfd88f994cf5237e9f08834c2f32b3
Endrit <endritzenuni112@gmail.com>
2022-04-22 21:14:58 -0400

    Fixed lobby and other stuff

--- New Unity Project/Assets/ConnectToServer.cs
+++ New Unity Project/Assets/ConnectToServer.cs
@@ -1,27 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
 using UnityEngine.SceneManagement;
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
+
+    public InputField usernameInput;
+    public Text buttonText;
     // Start is called before the first frame update
-    void Start()
-    {
-        PhotonNetwork.ConnectUsingSettings();
+    public void OnClickConnect(){
+        if (usernameInput.text.Length >= 1){
+            PhotonNetwork.NickName = usernameInput.text;
+            buttonText.text = "Connecting...";
+            PhotonNetwork.ConnectUsingSettings();
+        }
     }
 
     public override void OnConnectedToMaster(){
-        PhotonNetwork.JoinLobby();
-    }
-
-    public override void OnJoinedLobby(){
         SceneManager.LoadScene("Lobby");
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
